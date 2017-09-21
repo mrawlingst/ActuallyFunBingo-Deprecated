5116129c4ed79306813021d3fff4a39fd34e81f3
mrawlingst <mrawlingst@gmail.com>
2017-09-20 22:44:41 -0400

    Highlight cols

--- scripts/column_header.gd
+++ scripts/column_header.gd
@@ -4,3 +4,11 @@ export var col_num = 0
 
 func _ready():
 	get_node("Label").set_text("COL " + str(col_num))
+
+func _on_Column_mouse_enter():
+	for i in range(5):
+		get_node("../Milestone_" + str(i * 5 + col_num)).set_modulate(Color(.8, .8, .8))
+
+func _on_Column_mouse_exit():
+	for i in range(5):
+		get_node("../Milestone_" + str(i * 5 + col_num)).set_modulate(Color(1, 1, 1))
