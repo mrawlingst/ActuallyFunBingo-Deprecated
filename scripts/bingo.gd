a2e5188f4d7f8a73d7fd3304f28627ea8b5507f8
mrawlingst <mrawlingst@gmail.com>
2017-09-21 13:47:24 -0400

    Curate WIP

--- scripts/bingo.gd
+++ scripts/bingo.gd
@@ -17,10 +17,30 @@ func populate_card():
 	
 	while milestones.size() < 25:
 		var milestone = ""
-		while milestone == "" or milestones.has(milestone):
+		var index = 0
+		while milestone == "" or milestones.has(milestone) or curate(milestone, wow_data.milestones):
 			if bingo_info.game == "World of Warcraft":
-				milestone = wow_data.milestones.keys()[randi() % wow_data.milestones.size()]
+				index = randi() % wow_data.milestones.size()
+				milestone = wow_data.milestones.keys()[index]
+				print(milestone)
 		milestones.append(milestone)
 	
 	for i in range(25):
 		get_node("Card/Milestone_" + str(i + 1) + "/Label").set_text(milestones[i])
+
+func curate(milestone, game_milestones):
+	if game_milestones[milestone] == 0:
+		return false
+	
+	# COL CHECK
+	for col in range(5):
+		for row in range(5):
+			var milestone_check =get_node("Card/Milestone_" + str(row * 5 + col + 1) + "/Label").get_text()
+			if milestone_check == "Milestone":
+				continue
+			
+			if game_milestones[milestone] == game_milestones[milestone_check]:
+				print(milestone + " vs " + milestone_check)
+				return true
+
+	return false
