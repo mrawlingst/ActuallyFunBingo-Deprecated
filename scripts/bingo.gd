24dcceb433f4e7a9ed2180ff98a2214572de1475
mrawlingst <mrawlingst@gmail.com>
2017-09-21 00:23:09 -0400

    Populate card with wow data

--- scripts/bingo.gd
+++ scripts/bingo.gd
@@ -2,3 +2,15 @@ extends Node
 
 func _ready():
 	get_node("Title").set_text(bingo_info.game + " Bingo")
+	get_node("Seed Generator").new_seed()
+	populate_card()
+
+func populate_card():
+	var bingo_seed = get_node("Seed Generator").get_seed()
+	
+	print(bingo_seed)
+	seed(bingo_seed)
+	
+	if bingo_info.game == "World of Warcraft":
+		for i in range(25):
+			get_node("Card/Milestone_" + str(i + 1) + "/Label").set_text(wow_data.milestones.keys()[randi() % wow_data.milestones.size()])
--- scripts/seed_generator.gd
+++ scripts/seed_generator.gd
@@ -6,15 +6,20 @@ func _ready():
 	pass
 
 func _on_Generate_Button_pressed():
-	randomize()
 	if get_node("Seed Editbox").get_text().empty():
-		bingo_seed = randi() % 999999 + 1
+		new_seed()
 	else:
 		bingo_seed = int(get_node("Seed Editbox").get_text())
+	
+	get_node("../.").populate_card()
 
 func _on_Reset_Button_pressed():
 	for i in range(25):
 		get_node(str("../Card/Milestone_", i + 1)).set_pressed(false)
+
+func new_seed():
+	randomize()
+	bingo_seed = randi() % 999999 + 1
 
 func get_seed():
 	return bingo_seed
