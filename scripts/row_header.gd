48028ab8186dd8e44d5473391c5bbd07597164fb
mrawlingst <mrawlingst@gmail.com>
2017-09-20 22:46:52 -0400

    Highlight rows

--- scripts/row_header.gd
+++ scripts/row_header.gd
@@ -4,3 +4,11 @@ export var row_num = 0
 
 func _ready():
 	get_node("Label").set_text("ROW " + str(row_num))
+
+func _on_Row_mouse_enter():
+	for i in range(5):
+		get_node("../Milestone_" + str((i + 1) + 5 * (row_num - 1))).set_modulate(Color(.8, .8, .8))
+
+func _on_Row_mouse_exit():
+	for i in range(5):
+		get_node("../Milestone_" + str((i + 1) + 5 * (row_num - 1))).set_modulate(Color(1, 1, 1))
