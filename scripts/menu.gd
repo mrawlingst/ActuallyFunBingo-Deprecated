ee3c0632d7b9d1e3b8c07bf9d3b80635a9b80eda
mrawlingst <mrawlingst@gmail.com>
2017-09-21 00:08:11 -0400

    Change scene to bingo from menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
-func _ready():
-	pass
-
 func _on_Quit_Button_pressed():
 	get_tree().quit()
+
+func _on_World_of_Warcraft_pressed():
+	bingo_info.game = "World of Warcraft"
+	get_tree().change_scene("res://scenes/scene_bingo.tscn")
