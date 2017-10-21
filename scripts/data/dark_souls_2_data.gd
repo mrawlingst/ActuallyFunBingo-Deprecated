bf7120acec15ea3c301766e4c28ce1a8ed85640b
mrawlingst <mrawlingst@gmail.com>
2017-10-21 00:49:41 -0400

    Start DS2 - classes

---
+++ scripts/data/dark_souls_2_data.gd
@@ -0,0 +1,58 @@
+extends Node
+
+# Version of game data
+var version = "1.0.0"
+
+var milestones = {
+	# Non-exclusive
+	"Milestone 1": 0,
+	"Milestone 2": 0,
+	"Milestone 3": 0,
+	"Milestone 4": 0,
+	"Milestone 5": 0,
+	"Milestone 6": 0,
+	"Milestone 7": 0,
+	"Milestone 8": 0,
+	"Milestone 9": 0,
+	"Milestone 10": 0,
+	"Milestone 11": 0,
+	"Milestone 12": 0,
+	"Milestone 13": 0,
+	"Milestone 14": 0,
+	"Milestone 15": 0,
+	"Milestone 16": 0,
+	"Milestone 17": 0,
+	"Milestone 18": 0,
+	"Milestone 19": 0,
+	"Milestone 20": 0,
+	"Milestone 21": 0,
+	"Milestone 22": 0,
+	"Milestone 23": 0,
+	"Milestone 24": 0,
+	"Milestone 25": 0,
+
+	# Exclusive Group 1
+	"Milestone 26": 1,
+	"Milestone 27": 1,
+
+	# Exclusive Group 2
+	"Milestone 28": 2,
+	"Milestone 29": 2,
+}
+
+# Dark Souls 2 Classes
+var ds2_classes = [
+	"Bandit",
+	"Cleric",
+	"Deprived",
+	"Explorer",
+	"Knight",
+	"Sorcerer",
+	"Swordsman",
+	"Warrior"
+]
+
+
+# Return game specfic info for info label
+func info():
+	return ""
