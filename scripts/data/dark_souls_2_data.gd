d0f02ed9cc9c788e1867eb3293863ebfd3f6de49
mrawlingst <mrawlingst@gmail.com>
2017-10-21 01:43:37 -0400

    DS2 Bonfire milestones

--- scripts/data/dark_souls_2_data.gd
+++ scripts/data/dark_souls_2_data.gd
@@ -4,40 +4,119 @@ extends Node
 var version = "1.0.0"
 
 var milestones = {
-	# Non-exclusive
-	"Milestone 1": 0,
-	"Milestone 2": 0,
-	"Milestone 3": 0,
-	"Milestone 4": 0,
-	"Milestone 5": 0,
-	"Milestone 6": 0,
-	"Milestone 7": 0,
-	"Milestone 8": 0,
-	"Milestone 9": 0,
-	"Milestone 10": 0,
-	"Milestone 11": 0,
-	"Milestone 12": 0,
-	"Milestone 13": 0,
-	"Milestone 14": 0,
-	"Milestone 15": 0,
-	"Milestone 16": 0,
-	"Milestone 17": 0,
-	"Milestone 18": 0,
-	"Milestone 19": 0,
-	"Milestone 20": 0,
-	"Milestone 21": 0,
-	"Milestone 22": 0,
-	"Milestone 23": 0,
-	"Milestone 24": 0,
-	"Milestone 25": 0,
+	# BONFIRES
+	"Light bonfre (Lower Earthen Peak)": 0,
+	"Light bonfire (Central Earthen Peak)": 0,
+	"Light bonfire (Upper Earthen Peak)": 0,
+	
+	"Light bonfire (Tower of Prayer)": 0,
+	"Light bonfire (Crumbled Ruins)": 0,
+	"Light bonfire (Rhoy's Resting Place)": 0,
+	"Light bonfire (Rise of the Dead)": 0,
+	
+	"Light bonfire (Fire Keepers' Dwelling)": 0,
 
-	# Exclusive Group 1
-	"Milestone 26": 1,
-	"Milestone 27": 1,
+	"Light bonfire (The Crestfallen's Retreat)": 0,
+	"Light bonfire (Cardinal Tower)": 0,
+	"Light bonfire (Soldier's Rest)": 0,
+	"Light bonfire (The Place Unbeknownst)": 0,
+	
+	"Light bonfire (Threshold Bridge)": 0,
+	"Light bonfire (Ironhearth Hall)": 0,
+	"Light bonfire (Eygil's Idol)": 0,
+	
+	"Light bonfire (Undead Crypt Entrance)": 0,
+	"Light bonfire (Undead Ditch)": 0,
 
-	# Exclusive Group 2
-	"Milestone 28": 2,
-	"Milestone 29": 2,
+	"Light bonfire (Belfry Sol Approach)": 0,
+	
+	"Light bonfire (Foregarden)": 0,
+	"Light bonfire (Ritual Site)": 0,
+
+	"Light bonfire (Heide's Ruin)": 0,
+	"Light bonfire (Tower of Flame)": 0,
+	
+	"Light bonfire (Old Akelarre)": 0,
+	"Light bonfire (Ruined Fork Road)": 0,
+	"Light bonfire (Shaded Ruins)": 0,
+	
+	"Light bonfire (Dragon Aerie)": 0,
+
+	"Light bonfire (The Blue Catherdral)": 0,
+	
+	"Light bonfire (Dragon Shrine)": 0,
+	"Light bonfire (Shrine Entrance)": 0,
+	
+	"Light bonfire (Unseen Path to Heide)": 0,
+	
+	"Light bonfire (Gyrm's Respite)": 0,
+	"Light bonfire (Ordeal's End)": 0,
+
+	"Light bonfire (Exile Holding Cells)": 0,
+	"Light bonfire (McDuff's Workshop)": 0,
+	"Light bonfire (Servants' Quarters)": 0,
+	"Light bonfire (Straid's Cell)": 0,
+	"Light bonfire (The Tower Apart)": 0,
+	
+	"Light bonfire (Shulva, Sanctum City)": 0,
+	"Light bonfire (Sanctum Walk)": 0,
+	"Light bonfire (Tower of Prayer)": 0,
+	"Light bonfire (Priestess' Chamber)": 0,
+	"Light bonfire (Dragon's Sanctum)": 0,
+	"Light bonfire (Hidden Sanctum Chamber)": 0,
+	"Light bonfire (Lair of the Imperfect)": 0,
+	"Light bonfire (Sanctum Interior)": 0,
+	
+	"Light bonfire (Royal Army Campsite)": 0,
+	"Light bonfire (Chapel Threshold)": 0,
+	"Light bonfire (Lower Brightstone Cove)": 0,
+
+	"Light bonfire (Harval's Resting Place)": 0,
+	"Light bonfire (Grave Entrance)": 0,
+	
+	"Light bonfire (The Saltfort)": 0,
+	
+	"Light bonfire (Dragon's Rest)": 0,
+	"Light bonfire (Sanctum Nadir)": 0,
+
+	"Light bonfire (Upper Ramparts)": 0,
+	
+	"Light bonfire (Upper Gutter)": 0,
+	"Light bonfire (Central Gutter)": 0,
+	
+	"Light bonfire (Brume Tower)": 0,
+	"Light bonfire (Throne Floor)": 0,
+	"Light bonfire (Upper Floor)": 0,
+	"Light bonfire (Foyer)": 0,
+	"Light bonfire (Lowermost Floor)": 0,
+	"Light bonfire (Smelter Throne)": 0,
+	
+	"Light bonfire (Undead Refuge)": 0,
+	"Light bonfire (Bridge Approach)": 0,
+	"Light bonfire (Undead Lockaway)": 0,
+	
+	"Light bonfire (Black Gulch Mouth)": 0,
+	"Light bonfire (Hidden Chamber)": 0,
+
+	"Light bonfire (Iron Passage)": 0,
+	"Light bonfire (Iron Hallway Entrance)": 0,
+	
+	"Light bonfire (Undead Purgatory)": 0,
+	
+	"Light bonfire (King's Gate)": 0,
+	"Light bonfire (Under Castle Drangleic)": 0,
+	"Light bonfire (Forgotten Chamber)": 0,
+	"Light bonfire (Central Castle Drangleic)": 0,
+
+	"Light bonfire (Poison Pool)": 0,
+	"Light bonfire (The Mines)": 0,
+
+	"Light bonfire (Outer Wall)": 0,
+	"Light bonfire (Abandoned Dwelling)": 0,
+	"Light bonfire (Expulsion Chamber)": 0,
+	"Light bonfire (Inner Wall)": 0,
+	"Light bonfire (Lower Garrison)": 0,
+	"Light bonfire (Grand Cathedral)": 0,
 }
 
 # Dark Souls 2 Classes
