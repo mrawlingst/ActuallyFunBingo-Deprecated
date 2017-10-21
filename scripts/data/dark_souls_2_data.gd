24ad49d89536eb61770c518c13c97bc0fac6ec93
mrawlingst <mrawlingst@gmail.com>
2017-10-21 12:50:13 -0400

    Uncomment other milestones

--- scripts/data/dark_souls_2_data.gd
+++ scripts/data/dark_souls_2_data.gd
@@ -83,225 +83,225 @@ var milestones = {
 	"Light bonfire (Grand Cathedral)": 0,
 
 	# COVENANTS - 1
-	# "Join Bell Keepers": 1,
-	# "Join Blue Sentinels": 1,
-	# "Join Brotherhood of Blood": 1,
-	# "Join Rat King Covenant": 1,
-	# "Join Dragon Remnants": 1,
-	# "Join Way of Blue": 1,
-	# "Join Pilgrims of Dark": 1,
-	# "Join Heirs of the Sun": 1,
+	"Join Bell Keepers": 1,
+	"Join Blue Sentinels": 1,
+	"Join Brotherhood of Blood": 1,
+	"Join Rat King Covenant": 1,
+	"Join Dragon Remnants": 1,
+	"Join Way of Blue": 1,
+	"Join Pilgrims of Dark": 1,
+	"Join Heirs of the Sun": 1,
 
-	# # ATTRIBUTES - 2
-	# "36 Vigor": 2,
-	# "36 Endurance": 2,
-	# "36 Vitality": 2,
-	# "36 Attunement": 2,
-	# "36 Strength": 2,
-	# "36 Dexterity": 2,
-	# "36 Adaptability": 2,
-	# "36 Intelligence": 2,
-	# "36 Faith": 2,
+	# ATTRIBUTES - 2
+	"36 Vigor": 2,
+	"36 Endurance": 2,
+	"36 Vitality": 2,
+	"36 Attunement": 2,
+	"36 Strength": 2,
+	"36 Dexterity": 2,
+	"36 Adaptability": 2,
+	"36 Intelligence": 2,
+	"36 Faith": 2,
 
-	# # MYTHA, THE BANEFUL QUEEN - 3
-	# "Mytha, The Baneful Queen Soul": 3,
-	# "Mytha's Bent Blade": 3,
+	# MYTHA, THE BANEFUL QUEEN - 3
+	"Mytha, The Baneful Queen Soul": 3,
+	"Mytha's Bent Blade": 3,
 
-	# # SMELTER DEMON - 4
-	# "Smelter Demon Soul": 4,
-	# "Smelter Sword": 4,
-	# "Aged Smelter Sword": 4,
+	# SMELTER DEMON - 4
+	"Smelter Demon Soul": 4,
+	"Smelter Sword": 4,
+	"Aged Smelter Sword": 4,
 	
-	# # PURSUER - 5
-	# "Soul of the Pursuer": 5,
-	# "Pursuer's Ultra Greatsword": 5,
-	# "Pursuer's Greatshield": 5,
+	# PURSUER - 5
+	"Soul of the Pursuer": 5,
+	"Pursuer's Ultra Greatsword": 5,
+	"Pursuer's Greatshield": 5,
 	
-	# # FLEXILE SENTRY - 6
-	# "Flexile Sentry Soul": 6,
-	# "Warped Sword": 6,
-	# "Arced Sword": 6,
-	# "Barbed Club": 6,
+	# FLEXILE SENTRY - 6
+	"Flexile Sentry Soul": 6,
+	"Warped Sword": 6,
+	"Arced Sword": 6,
+	"Barbed Club": 6,
 
-	# # LAST GIANT - 7
-	# "Soul of the Last Giant": 7,
-	# "Giant Stone Axe": 7,
+	# LAST GIANT - 7
+	"Soul of the Last Giant": 7,
+	"Giant Stone Axe": 7,
 
-	# # BELFRY GARGOYLE - 8
-	# "Belfry Gargoyle Soul": 8,
-	# "Gargoyle Bident": 8,
+	# BELFRY GARGOYLE - 8
+	"Belfry Gargoyle Soul": 8,
+	"Gargoyle Bident": 8,
 
-	# # DRAGONRIDER - 9
-	# "Dragonrider Soul": 9,
-	# "Dragonrider's Halberd": 9,
-	# "Dragonrider Twinblade": 9,
-	# "Dragonrider Bow": 9,
-	# "Dragonrider Greatshield": 9,
+	# DRAGONRIDER - 9
+	"Dragonrider Soul": 9,
+	"Dragonrider's Halberd": 9,
+	"Dragonrider Twinblade": 9,
+	"Dragonrider Bow": 9,
+	"Dragonrider Greatshield": 9,
 
-	# # SKELETON LORD - 10
-	# "Skeleton Lord's Soul": 10,
-	# "Roaring Halberd": 10,
+	# SKELETON LORD - 10
+	"Skeleton Lord's Soul": 10,
+	"Roaring Halberd": 10,
 
-	# # COVETOUS DEMON - 11
-	# "Covetous Demon Soul": 11,
-	# "Bone Scythe": 11,
+	# COVETOUS DEMON - 11
+	"Covetous Demon Soul": 11,
+	"Bone Scythe": 11,
 	
-	# # EXECUTIONER'S CHARIOT - 12
-	# "Executioner's Chariot Soul": 12,
-	# "Chariot Lance": 12,
-	# "Shield Crossbow": 12,
+	# EXECUTIONER'S CHARIOT - 12
+	"Executioner's Chariot Soul": 12,
+	"Chariot Lance": 12,
+	"Shield Crossbow": 12,
 
-	# # DEMON OF SONG - 13
-	# "Demon of Song Soul": 13,
-	# "Spotted Whip": 13,
+	# DEMON OF SONG - 13
+	"Demon of Song Soul": 13,
+	"Spotted Whip": 13,
 
-	# # RUIN SENTINEL - 14
-	# "Ruin Sentinel Soul": 14,
-	# "Heavy Homing Soul Arrow (S)": 14,
+	# RUIN SENTINEL - 14
+	"Ruin Sentinel Soul": 14,
+	"Heavy Homing Soul Arrow (S)": 14,
 
-	# # SCORPIONESS NAJKA - 15
-	# "Scorpioness Najka Soul": 15,
-	# "Soul Shower (S)": 15,
+	# SCORPIONESS NAJKA - 15
+	"Scorpioness Najka Soul": 15,
+	"Soul Shower (S)": 15,
 
-	# # VELSTADT - 16
-	# "Soul of Velstadt": 16,
-	# "Sacred Oath (M)": 16,
-	# "Sacred Chime Hammer": 16,
+	# VELSTADT - 16
+	"Soul of Velstadt": 16,
+	"Sacred Oath (M)": 16,
+	"Sacred Chime Hammer": 16,
 
-	# # ROYAL RAT VANGUARD - 17
-	# "Royal Rat Vanguard Soul": 17,
-	# "Toxic Mist (P)": 17,
+	# ROYAL RAT VANGUARD - 17
+	"Royal Rat Vanguard Soul": 17,
+	"Toxic Mist (P)": 17,
 
-	# # ROYAL RAT AUTHORITY - 18
-	# "Royal Rat Authority Soul": 18,
-	# "Acid Surge (P)": 18,
+	# ROYAL RAT AUTHORITY - 18
+	"Royal Rat Authority Soul": 18,
+	"Acid Surge (P)": 18,
 
-	# # GIANT LORD - 19
-	# "Giant Lord Soul": 19,
-	# "Repel (H)": 19,
+	# GIANT LORD - 19
+	"Giant Lord Soul": 19,
+	"Repel (H)": 19,
 
-	# # OLD WITCH - 20
-	# "Old Witch Soul": 20,
-	# "Flame Weapon (P)": 20,
-	# "Chaos Blade": 20,
+	# OLD WITCH - 20
+	"Old Witch Soul": 20,
+	"Flame Weapon (P)": 20,
+	"Chaos Blade": 20,
 
-	# # OLD PALEDRAKE - 21
-	# "Old Paledrake Soul": 21,
-	# "Crystal Soul Spear (S)": 21,
-	# "Moonlight Greatsword": 21,
+	# OLD PALEDRAKE - 21
+	"Old Paledrake Soul": 21,
+	"Crystal Soul Spear (S)": 21,
+	"Moonlight Greatsword": 21,
 
-	# # OLD KING - 22
-	# "Old King Soul": 22,
-	# "Blinding Bolt (M)": 22,
-	# "Dragonslayer Greatbow": 22,
+	# OLD KING - 22
+	"Old King Soul": 22,
+	"Blinding Bolt (M)": 22,
+	"Dragonslayer Greatbow": 22,
 	
-	# # OLD DEAD ONE - 23
-	# "Old Dead One Soul": 23,
-	# "Numbness (H)": 23,
-	# "Crypt Blacksword": 23,
+	# OLD DEAD ONE - 23
+	"Old Dead One Soul": 23,
+	"Numbness (H)": 23,
+	"Crypt Blacksword": 23,
 
-	# # DARKLURKER - 24
-	# "Darklurker Soul": 24,
-	# "Lifedrain Patch (H)": 24,
+	# DARKLURKER - 24
+	"Darklurker Soul": 24,
+	"Lifedrain Patch (H)": 24,
 
-	# # NADALIA, BRIDE OF ASH - 25
-	# "Soul of Nadalia, Bride of Ash": 25,
-	# "Outcry (P)": 25,
-	# "Chime of Screams": 25,
+	# NADALIA, BRIDE OF ASH - 25
+	"Soul of Nadalia, Bride of Ash": 25,
+	"Outcry (P)": 25,
+	"Chime of Screams": 25,
 
-	# # LOOKING GLASS KNIGHT - 26
-	# "Looking Glass Knight Soul": 26,
-	# "Thorned Greatsword": 26,
-	# "King's Mirror": 26,
+	# LOOKING GLASS KNIGHT - 26
+	"Looking Glass Knight Soul": 26,
+	"Thorned Greatsword": 26,
+	"King's Mirror": 26,
 
-	# # LOST SINNER - 27
-	# "Soul of the Lost Sinner": 27,
-	# "Lost Sinner's Sword": 27,
+	# LOST SINNER - 27
+	"Soul of the Lost Sinner": 27,
+	"Lost Sinner's Sword": 27,
 
-	# # DUKE'S DEAR FREJA - 28
-	# "Soul of the Duke's Dear Freja": 28,
-	# "Spider Fang": 28,
-	# "Spider's Silk": 28,
+	# DUKE'S DEAR FREJA - 28
+	"Soul of the Duke's Dear Freja": 28,
+	"Spider Fang": 28,
+	"Spider's Silk": 28,
 
-	# # ROTTEN - 29
-	# "Soul of the Rotten": 29,
-	# "Butcher's Knife": 29,
+	# ROTTEN - 29
+	"Soul of the Rotten": 29,
+	"Butcher's Knife": 29,
 
-	# # OLD IRON KING - 30
-	# "Old Iron King Soul": 30,
-	# "Iron King Hammer": 30,
+	# OLD IRON KING - 30
+	"Old Iron King Soul": 30,
+	"Iron King Hammer": 30,
 
-	# # KING - 31
-	# "Soul of the King": 31,
-	# "Ruler's Sword": 31,
-	# "King's Shield": 31,
-	# "King's Ultra Greatsword": 31,
+	# KING - 31
+	"Soul of the King": 31,
+	"Ruler's Sword": 31,
+	"King's Shield": 31,
+	"King's Ultra Greatsword": 31,
 
-	# # GUARDIAN DRAGON - 32
-	# "Guardian Dragon Soul": 32,
-	# "Drakewing Ultra Greatsword": 32,
-	# "Spitfire Spear": 32,
+	# GUARDIAN DRAGON - 32
+	"Guardian Dragon Soul": 32,
+	"Drakewing Ultra Greatsword": 32,
+	"Spitfire Spear": 32,
 
-	# # ANCIENT DRAGON - 33
-	# "Ancient Dragon Soul": 33,
-	# "Curved Dragon Greatsword": 33,
+	# ANCIENT DRAGON - 33
+	"Ancient Dragon Soul": 33,
+	"Curved Dragon Greatsword": 33,
 
-	# # THRONE WATCHER - 34
-	# "Throne Watcher Soul": 34,
-	# "Watcher Greatsword": 34,
-	# "Watcher's Shield": 34,
+	# THRONE WATCHER - 34
+	"Throne Watcher Soul": 34,
+	"Watcher Greatsword": 34,
+	"Watcher's Shield": 34,
 
-	# # THRONE DEFENDER - 35
-	# "Throne Defender Soul": 35,
-	# "Defender Greatsword": 35,
-	# "Defender's Shield": 35,
+	# THRONE DEFENDER - 35
+	"Throne Defender Soul": 35,
+	"Defender Greatsword": 35,
+	"Defender's Shield": 35,
 
-	# # NASHANDRA - 36
-	# "Soul of Nashandra": 36,
-	# "Scythe of Want": 36,
-	# "Chime of Want": 36,
-	# "Bow of Want": 36,
+	# NASHANDRA - 36
+	"Soul of Nashandra": 36,
+	"Scythe of Want": 36,
+	"Chime of Want": 36,
+	"Bow of Want": 36,
 
-	# # OLD DRAGONSLAYER - 37
-	# "Old Dragonslayer Soul": 37,
-	# "Dragonslayer Spear": 37,
+	# OLD DRAGONSLAYER - 37
+	"Old Dragonslayer Soul": 37,
+	"Dragonslayer Spear": 37,
 
-	# # FUME KNIGHT - 38
-	# "Soul of the Fume Knight": 38,
-	# "Fume Sword": 38,
-	# "Fume Ultra Greatsword": 38,
+	# FUME KNIGHT - 38
+	"Soul of the Fume Knight": 38,
+	"Fume Sword": 38,
+	"Fume Ultra Greatsword": 38,
 
-	# # SIR ALONNE - 39
-	# "Soul of Sir Alonne": 39,
-	# "Bewitched Alonne Sword": 39,
+	# SIR ALONNE - 39
+	"Soul of Sir Alonne": 39,
+	"Bewitched Alonne Sword": 39,
 
-	# # AAVA, THE KING'S PET - 40
-	# "Soul of Aava's the King's Pet": 40,
-	# "Ivory Straight Sword": 40,
+	# AAVA, THE KING'S PET - 40
+	"Soul of Aava's the King's Pet": 40,
+	"Ivory Straight Sword": 40,
 
-	# # IVORY KING - 41
-	# "Soul of the Ivory King": 41,
-	# "Ivory King Ultra Greatsword": 41,
+	# IVORY KING - 41
+	"Soul of the Ivory King": 41,
+	"Ivory King Ultra Greatsword": 41,
 
-	# # ALSANNA, SILENT ORACLE - 42
-	# "Soul of Alsanna, Silent Oracle": 42,
-	# "Eleum Loyce": 42,
+	# ALSANNA, SILENT ORACLE - 42
+	"Soul of Alsanna, Silent Oracle": 42,
+	"Eleum Loyce": 42,
 
-	# # LUD, THE KING'S PET - 43
-	# "Soul of Lud, the King's Pet": 43,
-	# "Loyce Greatsword": 43,
+	# LUD, THE KING'S PET - 43
+	"Soul of Lud, the King's Pet": 43,
+	"Loyce Greatsword": 43,
 
-	# # ZALLEN, THE KING'S PET - 44
-	# "Soul of Zallen, the King's Pet": 44,
-	# "Loyce Shield": 44,
+	# ZALLEN, THE KING'S PET - 44
+	"Soul of Zallen, the King's Pet": 44,
+	"Loyce Shield": 44,
 
-	# # ELANA, SQUALID QUEEN - 45
-	# "Soul of Elana, Squalid Queen": 45,
-	# "Wrathful Axe": 45,
+	# ELANA, SQUALID QUEEN - 45
+	"Soul of Elana, Squalid Queen": 45,
+	"Wrathful Axe": 45,
 	
-	# # SINH, THE SLUMBERING DRAGON - 46
-	# "Soul of Sinh, the Slumbering Dragon": 46,
-	# "Yorgh's Spear": 46,
+	# SINH, THE SLUMBERING DRAGON - 46
+	"Soul of Sinh, the Slumbering Dragon": 46,
+	"Yorgh's Spear": 46,
 }
 
 # Dark Souls 2 Classes
